6808b74c832f975c6606a11ed3e43bb3a320fc91
Thiago Perrotta <tbperrotta@gmail.com>
2024-01-31 02:44:47 -0300

    add variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "github_username" {
+  type    = string
+  default = "thiagowfx"
+}
+
+variable "linode_hostname" {
+  type    = string
+  default = "coruscant"
+}
+
+variable "linode_region" {
+  type    = string
+  default = "eu-central"
+}
